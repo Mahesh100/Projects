a21a7f9d7e39ca02f205822b665e6cf8d5e1a7a2
Mahesh <maheshkadke100@gmail.com>
2021-12-05 21:00:58 +0530

    Initial Commit

---
+++ Real-Time-Chat-Application/client/src/components/TextContainer/TextContainer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+
+import './TextContainer.css';
+
+const TextContainer = ({ users }) => (
+  <div className="textContainer">
+    <div>
+      <h1>Realtime Chat Application <span role="img" aria-label="emoji">💬</span></h1>
+      <h1>Welcome To Awesome Chat Application. <span> </span> <span role="img" aria-label="emoji">❤️</span></h1>
+      <h1>Try it out right now! <span role="img" aria-label="emoji">⬅️</span></h1>
+
+      <h1>Start Messaging by Joining Room </h1> 
+
+    </div>
+    {
+      users
+        ? (
+          <div>
+            <h1>People currently chatting:</h1>
+            <div className="activeContainer">
+              <h2>
+                {users.map(({name}) => (
+                  <div key={name} className="activeItem">
+                    {name}
+                    <img alt="Online Icon" src={onlineIcon}/>
+                  </div>
+                ))}
+              </h2>
+            </div>
+          </div>
+        )
+        : null
+    }
+  </div>
+);
+
+export default TextContainer;
